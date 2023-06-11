5a5500d12f194b7bc3fd18cfae90fcc46546399a
¨Howard <programadorhaaj.2023@gmail.com>
2023-06-11 08:03:09 -0600

    creacion de las tablas

--- bd.sql
+++ bd.sql
@@ -1,7 +1,31 @@
-create table productos(
-    producto_id serial not null,
-    producto_nombre varchar(50) not null,
-    producto_precio decimal(10,2) not null,
-    producto_situacion smallint not null default 1,
-    primary key (producto_id)
-)
+
+CREATE TABLE
+    programadores (
+        id_programador serial PRIMARY KEY,
+        nombre VARCHAR(255) not null,
+        apellido VARCHAR(255),
+        telefono VARCHAR(255),
+        direccion VARCHAR(255),
+        correo_electronico VARCHAR(255)
+    );
+
+CREATE TABLE aplserial s (
+        id_aplicacion serial PRIMARY KEY,
+        nombre VARCHAR(255),
+    );
+
+CREATE TABLE
+    AsignacionAplicaciones (
+        id_asignacion serial PRIMARY KEY,
+        id_aplicacion INTEGER REFERENCES Aplicaciones(id_aplicacion),
+        id_programador INTEGER REFERENCES Programadores(id_programador)
+    );
+
+CREATE TABLE
+    Tareas (
+        id_tarea serial PRIMARY KEY,
+        id_aplicacion INTEGER REFERENCES Aplicaciones(id_aplicacion),
+        tarea VARCHAR(255),
+        fecha_realizacion DATE,
+        estado VARCHAR(255)
+    );
