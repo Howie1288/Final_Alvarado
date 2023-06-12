d1fd811635e5eb22211aec139a5b056095d00013
¨Howard <programadorhaaj.2023@gmail.com>
2023-06-12 15:15:00 -0600

    Correciones de errotr de Navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,58 +4,71 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a class="navbar-brand" href="#">Tienda virtual</a>
+            <a class="navbar-brand" href="#">REGISTRO DE APLICACIONES</a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Asignacion Aplicaciones
+                        APLICACIONES
                     </a>
                     <ul class="dropdown-menu bg-dark">
                         <li class="nav-item">
-                            <a class="nav-link" href="/Final_Alvarado/vistas/asignacion/index.php">Asignar Aplicaciones</a>
+                            <a class="nav-link" href="/Final_Alvarado/vistas/aplicaciones/index.php">Ingresar Aplicacion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/Final_Alvarado/vistas/aplicaciones/buscar.php">Buscar aplicacion</a>
                         </li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Aplicaciones
+                    PROGRAMADORES
                     </a>
                     <ul class="dropdown-menu bg-dark">
                         <li class="nav-item">
-                            <a class="nav-link" href="/Final_Alvarado/vistas/aplicaciones/index.php">Crear Aplicacion</a>
+                            <a class="nav-link" href="/Final_Alvarado/vistas/programadores/index.php">Ingresar programadores</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/Final_Alvarado/vistas/aplicaciones/buscar.php">Buscar Aplicacion</a>
+                            <a class="nav-link" href="/Final_Alvarado/vistas/programadores/buscar.php">Buscar programadores</a>
                         </li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Programadores
+                    ASIGNACION PROGRAMADOR
                     </a>
                     <ul class="dropdown-menu bg-dark">
                         <li class="nav-item">
-                            <a class="nav-link" href="/Final_Alvarado/vistas/programadores/index.php">Crear Programador</a>
+                            <a class="nav-link" href="/Final_Alvarado/vistas/asignacion/index.php">Asignar programadores a las APPS</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/Final_Alvarado/vistas/programadores/buscar.php">Buscar programador</a>
+                            <a class="nav-link" href="/Final_Alvarado/vistas/asignacion/buscar.php">Buscar programadores asignados a las APPS</a>
                         </li>
                     </ul>
                 </li>
-
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Tareas
+                       TAREAS APLICACIONES
                     </a>
                     <ul class="dropdown-menu bg-dark">
                         <li class="nav-item">
-                            <a class="nav-link" href="/Final_Alvarado/vistas/tareas/index.php">Crear Tareas</a>
+                            <a class="nav-link" href="/Final_Alvarado/vistas/tareas/index.php">Ingresar Tareas</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/Final_Alvarado/vistas/tareas/buscar.php">Buscar Tareas</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    PROGRESO APLICACIONES
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                           
+                            <a class="nav-link" href="/Final_Alvarado/vistas/progreso_aplicacion/progreso_aplicacion.php">Buscar el progreso de las aplicaciones</a>
                         </li>
                     </ul>
                 </li>
