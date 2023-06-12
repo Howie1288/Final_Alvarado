baaaf19b9458cda16fa80b388435c616548bf8ef
¨Howard <programadorhaaj.2023@gmail.com>
2023-06-12 12:21:43 -0600

    Eliminacion de la tabla Situacion

--- vistas/tareas/modificar.php
+++ vistas/tareas/modificar.php
@@ -1,8 +1,21 @@
 <?php
-require '../../modelos/Tareas.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once '../../modelos/Tareas.php';
+require_once '../../modelos/Aplicacion.php';
+
+try {
+    $tarea = new Tareas($_GET);
+    $tareas = $tarea->buscar();
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+} catch (Exception $e2) {
+    $error = $e2->getMessage();
+}
+
 try {
     $aplicacion = new Aplicacion($_GET);
-
     $aplicaciones = $aplicacion->buscar();
 } catch (PDOException $e) {
     $error = $e->getMessage();
@@ -10,54 +23,51 @@ try {
     $error = $e2->getMessage();
 }
 ?>
+
 <?php include_once '../../includes/header.php' ?>
 <div class="container">
     <h1 class="text-center">Modificar Tareas</h1>
     <div class="row justify-content-center">
         <form action="/Final_Alvarado/controladores/tareas/modificar.php" method="POST" class="col-lg-8 border bg-light p-3">
-            <input type="hidden" name="tarea_id" value="<?= $tareas[0]['tarea_id'] ?>">
+            <input type="hidden" name="tarea_id" value="<?= $tareas[0]['TAREA_ID'] ?>" >
             <div class="row mb-3">
                 <div class="col">
-                    <label for="tarea_id_aplicacion">ID APLICACION</label>
-                    <input type="text" name="tarea_id_aplicacion" id="tarea_id_aplicacion" class="form-control" value="<?= $tareas[0]['TAREA_ID_APLICACION'] ?>">
+                    <label for="tarea_id_aplicacion">Id Aplicación</label>
+                    <select name="tarea_id_aplicacion" id="tarea_id_aplicacion" class="form-control">
+                        <option value="">SELECCIONE...</option>
+                        <?php foreach ($aplicaciones as $key => $aplicacion) : ?>
+                            <option value="<?= $aplicacion['APLICACION_ID'] ?>"><?= $aplicacion['APLICACION_NOMBRE'] ?></option>
+                        <?php endforeach?>
+                    </select>
                 </div>
             </div>
-
             <div class="row mb-3">
                 <div class="col">
-                    <label for="tarea_descripcion">DESCRIPCION</label>
-                    <input type="text" name="tarea_descripcion" id="tarea_descripcion" class="form-control" value="<?= $tareas[0]['tarea_descripcion'] ?>">
+                    <label for="tarea_descripcion">Descripción </label>
+                    <textarea name="tarea_descripcion" id="tarea_descripcion" class="form-control" required></textarea>
                 </div>
             </div>
-
             <div class="row mb-3">
                 <div class="col">
-                    <label for="tarea_estado">ESTADO</label></label>
-                    <input type="text" name="tarea_estado" id="tarea_estado" class="form-control" value="<?= $tareas[0]['TAREA_ESTADO'] ?>">
+                    <label for="tarea_estado">Estado </label>
+                    <select name="tarea_estado" id="tarea_estado" class="form-control" required>
+                        <option value="FINALIZADA">Finalizada</option>
+                        <option value="NO INICIADA">No iniciada</option>
+                    </select>
                 </div>
             </div>
-
             <div class="row mb-3">
                 <div class="col">
-                    <label for="tarea_fecha">FECHA DE INICIO</label>
-                    <input type="date" name="tarea_fecha" id="tarea_fecha" class="form-control" value="<?= $tareas[0]['TAREA_FECHA'] ?>">
+                    <label for="tarea_fecha">Fecha </label>
+                    <input type="date" value="<?= date('Y-m-d') ?>" name="tarea_fecha" id="tarea_fecha" class="form-control" required>
                 </div>
             </div>
-
-
             <div class="row mb-3">
                 <div class="col">
-                    <label for="tarea_situacion">SITUACION</label>
-                    <input type="text" name="tarea_situacion" id="tarea_situacion" class="form-control" value="<?= $tareas[0]['TAREA_SITUACION'] ?>">
+                    <button type="submit" class="btn btn-warning w-100">Modificar</button>
                 </div>
             </div>
+        </form>
     </div>
-    <div class="row mb-3">
-        <div class="col">
-            <button type="submit" class="btn btn-warning w-100">Modificar</button>
-        </div>
-    </div>
-    </form>
-</div>
 </div>
 <?php include_once '../../includes/footer.php' ?>
