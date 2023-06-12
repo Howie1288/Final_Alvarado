62eb2c8426f04a921dd92f650b5220717ec231fc
¨Howard <programadorhaaj.2023@gmail.com>
2023-06-12 10:42:36 -0600

    Cambio del metodo a GET

--- vistas/programadores/index.php
+++ vistas/programadores/index.php
@@ -1,44 +1,39 @@
-<?php include_once '../../includes/header.php' ?>
-<?php include_once '../../includes/navbar.php' ?>
-<div class="container">
-    <h1 class="text-center">Formulario de ingreso de programadores</h1>
-    <div class="row justify-content-center">
-        <form action="/Final_Alvarado/controladores/Programadores/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
-            <div class="row mb-3">
-                <div class="col">
-                    <label for="programador_grado">Grado Militar del programador</label>
-                    <input type="text" name="programador_grado" id="programador_grado" class="form-control">
+<?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/navbar.php'?>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+    <div class="container">
+        <h1 class="text-center">Formulario de ingreso de programadores</h1>
+        <div class="row justify-content-center">
+            <form action="/Final_Alvarado/controladores/Programadores/guardar.php" method="GET" class="col-lg-8 border bg-light p-3">
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="programador_grado">Grado Militar del programador</label>
+                        <input type="text" name="programador_grado" id="programador_grado" class="form-control">
+                    </div>
                 </div>
-            </div>
-
-            <div class="row mb-3">
-                <div class="col">
-                    <label for="programador_nombre">Nombre del Programador</label>
-                    <input type="text" name="programador_nombre" id="programador_nombre" class="form-control">
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="programador_nombre">Nombre del Programador</label>
+                        <input type="text" name="programador_nombre" id="programador_nombre" class="form-control">
+                    </div>
                 </div>
-            </div>
-
-
-            <div class="row mb-3">
-                <div class="col">
-                    <label for="programador_apellido">Apellidos del programador</label>
-                    <input type="text" name="programador_apellido" id="programador_apellido" class="form-control">
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="programador_apellido">Apellidos del Programador</label>
+                        <input type="text" name="programador_apellido" id="programador_apellido" class="form-control">
+                    </div>
                 </div>
-            </div>
-
-          
-          
-          
-          
-           
-           
-
-            <div class="row mb-3">
-                <div class="col">
-                    <button type="submit" class="btn btn-primary w-100">Guardar</button>
+                <div class="row mb-3">
+                    <div class="col">
+                        <button type="submit" class="btn btn-info w-100">Buscar</button>
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
-</div>
-<?php include_once '../../includes/footer.php' ?>
+    <?php include_once '../../includes/footer.php'?>
