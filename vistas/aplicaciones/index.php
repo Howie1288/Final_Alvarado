9e6bc3bec2dbf94da704469cc609d4f40aeb8f63
¨Howard <programadorhaaj.2023@gmail.com>
2023-06-12 10:05:58 -0600

    Actualizacion de codigo de error

--- vistas/aplicaciones/index.php
+++ vistas/aplicaciones/index.php
@@ -1,23 +1,26 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
 <?php include_once '../../includes/header.php' ?>
 <?php include_once '../../includes/navbar.php' ?>
 <div class="container">
-    <h1 class="text-center">Formulario de Aplicaciones</h1>
+    <h1 class="text-center">Formulario de registro de aplicaciones</h1>
     <div class="row justify-content-center">
         <form action="/Final_Alvarado/controladores/aplicaciones/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
             <div class="row mb-3">
                 <div class="col">
-                    <label for="aplicacion_nombre">Nombre de la Aplicacion</label>
+                    <label for="aplicacion_nombre">Nombre de la aplicación</label>
                     <input type="text" name="aplicacion_nombre" id="aplicacion_nombre" class="form-control">
                 </div>
             </div>
             <div class="row mb-3">
                 <div class="col">
-                    <label for="aplicacion_fecha_inicio">Fecha de inicio Aplicacion</label>
+                    <label for="aplicacion_fecha_inicio">Fecha de inicio de la aplicación</label>
                     <input type="date" name="aplicacion_fecha_inicio" id="aplicacion_fecha_inicio" class="form-control">
                 </div>
             </div>
-
-
             <div class="row mb-3">
                 <div class="col">
                     <button type="submit" class="btn btn-primary w-100">Guardar</button>
@@ -26,4 +29,4 @@
         </form>
     </div>
 </div>
-<?php include_once '../../includes/footer.php' ?>
+<?php include_once '../../includes/footer.php'?>
